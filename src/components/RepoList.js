8e25d2d47f9d77247c27185f60c9eb02a258a807
Kyle Richardson <kyle.richardson1@gmail.com>
2021-06-15 16:13:15 -0500

    handles url username, catching invalid.

--- src/components/RepoList.js
+++ src/components/RepoList.js
@@ -12,7 +12,6 @@ const RepoList = ({userObj}) => {
     const getRepos = async () => {
         try {
           const repos = await axios.get(`https://api.github.com/users/${userObj.login}/repos`)
-          console.log(repos.data)
           setRepoList(repos.data)
         }
         catch(err) {
@@ -24,7 +23,8 @@ const RepoList = ({userObj}) => {
         <div className="repo-list-container">
           {repoList && <h1 className="repo-list-header">Showing public repositories for <a href={userObj.html_url} target="_blank" title="Open this users github in a new tab" >{userObj.login}</a></h1>}
           <div>
-            {repoList && repoList.map(repo => <RepoItem key = {repo.id} repo = {repo}/>)}
+            {repoList && repoList.length === 0 && <p style={{textAlign:"center"}}>No public repositories for this user</p>}
+            {repoList && repoList.length > 0 && repoList.map(repo => <RepoItem key = {repo.id} repo = {repo}/>)}
           </div>
         </div>
 
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,25 +1,34 @@
 
-import React, {useState} from "react"
+import React, {useEffect, useState} from "react"
 import axios from "axios"
 
-const Search = ({setUserObj}) => {
-    const [search, setSearch] = useState()
+const Search = ({setUserObj, path, setError}) => {
+    const [search, setSearch] = useState(path ? path : null)
+    useEffect(()=> {
+        if(path) {
+            getUser(path)
+        }
+    }, [path])
     const handleChange = (e) => {
         const {value} = e.target
         setSearch(value)
     }
     const handleSubmit = (e) => {
         e.preventDefault()
-        getUser()
+        if(search) {
+            window.location.pathname = `/${search}`
+        }
+       
     }
-    const getUser = async () => {
+    const getUser = async (str) => {
         try {
-            const user = await axios.get(`https://api.github.com/users/${search}`)
+            const user = await axios.get(`https://api.github.com/users/${str}`)
             console.log(user.data)
             setUserObj(user.data)
         }
         catch(err) {
             console.log(err)
+            setError(`${str} is not a valid Github username.  Please try a different username.`)
         }
     }
     return (
